650200bf02031842374338129ca4d2872baf6d07
Luis Santos <luis.santos@dominiondms.com>
2026-05-04 19:06:26 -0500

    Add url

--- AutoServiceManager.Api/Program.cs
+++ AutoServiceManager.Api/Program.cs
@@ -17,7 +17,8 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:5173",
-                "http://localhost:5174"
+                "http://localhost:5174",
+                "http://localhost:4200"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
